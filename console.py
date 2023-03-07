26642f6136d87de9f970e6df15ea895863e3b8c8
yaekobbonke <yaekobbundure.com>
2023-03-07 11:03:47 -0800

    console first part done

---
+++ console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, line):
+        "Quit command to exit the program"
+        return True
+    def do_EOF(self, args):
+        "ends the program"
+        return True
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
